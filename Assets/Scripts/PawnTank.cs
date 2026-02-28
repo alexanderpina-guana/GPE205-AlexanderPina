ce982de1d6fed95033ee7a5ac56dde26658ed77d
alexanderpina-guana <pinaa5533@gmail.com>
2026-02-28 09:54:41 -0700

    Update PawnTank.cs

--- Assets/Scripts/PawnTank.cs
+++ Assets/Scripts/PawnTank.cs
@@ -2,6 +2,27 @@ using UnityEngine;
 
 public class PawnTank : Pawn
 {
+
+    //variable that connects pawntank to shooter
+    private ShooterTank shooter;
+
+    //variable for the shoot force
+    public float shootForce;
+
+    //places the player tank
+    public override void Start()
+    {
+        GameManager.instance.tanks.Add(this);
+        shooter = GetComponent<ShooterTank>();
+        base.Start();
+    }
+
+//destroys tank when they lose all their health
+    public void OnDestroy()
+    {
+        GameManager.instance.tanks.Remove(this);
+    }
+    
     //A pawn function that tells the mover to move the tank
     public override void Move(Vector3 directionToMove)
     {
@@ -17,8 +38,9 @@ public class PawnTank : Pawn
     //Currently unused shoot pawn function
     public override void Shoot()
     {
-        
+        shooter.Shoot();
     }
 
 
 }
+
