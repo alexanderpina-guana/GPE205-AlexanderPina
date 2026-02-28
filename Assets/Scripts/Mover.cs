039561de60c261e394c0dec17d3f312be4ed639a
alexanderpina-guana <pinaa5533@gmail.com>
2026-02-28 09:38:47 -0700

    Update Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,4 +7,7 @@ public abstract class Mover : MonoBehaviour
 
    //Variable that makes the tank rotate
    public abstract void Rotate(Vector3 rotateDirection, float turnSpeed);
+   
+   public abstract void RotateTowards(Vector3 position, float turnSpeed);
 }
+
